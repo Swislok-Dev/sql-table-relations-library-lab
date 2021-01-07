3bb64c26d1843c0fab888b965f383014b05dcce9
Cory M <cmarker4@gmail.com>
2021-01-07 09:03:51 -0600

    commit tree 6816c87d7a70b168bedb371f1f002995d9b220f6

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,11 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name
+  "SELECT characters.name, COUNT(books.id) AS number_of_books
   FROM characters
-  JOIN "
+  JOIN character_books ON character_books.character_id = characters.id
+  JOIN books ON books.id = character_books.book_id
+  GROUP BY characters.name
+  ORDER BY number_of_books DESC, characters.name
+  ;"
 end
